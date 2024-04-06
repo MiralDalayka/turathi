4a6e5057f33fcc7a9932a0f5b8fa5c4362920af5
Alaa  Jamal Abdalqader <162041062+AlaaJam@users.noreply.github.com>
2024-04-06 04:56:41 +0300

    fire

--- lib/core/models/testModel.dart
+++ lib/core/models/testModel.dart
@@ -48,53 +48,8 @@ class TestList {
     return TestList(tests: tempTests);
   }
 }
-import 'dart:developer';
 
 
-class TestModel {
-String? title;
-int? num;
-
-  TestModel(
-      {
-
-      required  this.title,
-        required  this.num,
-
-       });
-
-  TestModel.fromJson(Map<String, dynamic> json) {
-
-    title = json['title'];
-    num = json['num'];
 
 
-  }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-
-    data['title'] = this.title;
-    data['num'] = this.num;
-
-    return data;
-  }
-}
-
-
-class TestList {
-  List<TestModel> tests;
-
-  TestList({required this.tests});
-
-  factory TestList.fromJson(List<dynamic> data) {
-    log("I'm In TestList Factory");
-    //1. temp list
-    List<TestModel> tempTests = [];
-    tempTests = data.map((item) {
-      return TestModel.fromJson(Map<String, dynamic>.from(item));
-    }).toList();
-
-    return TestList(tests: tempTests);
-  }
-}
