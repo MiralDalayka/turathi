7a6bcb49c378f989899adfcfd5d526efa60d3756
Alaa  Jamal Abdalqader <162041062+AlaaJam@users.noreply.github.com>
2024-05-22 22:49:58 +0300

    Add comments to router

--- lib/utils/Router/const_router_names.dart
+++ lib/utils/Router/const_router_names.dart
@@ -1,44 +1,56 @@
+// Initial route for the app
 const String initRoute = '/';
+
+// Route for the bottom navigation screen
 const String bottomNavRoute = '/bottomScreen';
+
+// Routes for different sections under the bottom navigation screen
 const String homeRoute = '/bottomScreen/home';
 const String eventsRoute = '/bottomScreen/home/EventsScreen';
 const String addNewEventRoute = '/bottomScreen/home/AddNewEvent';
 const String addNewPlaceRoute = '/bottomScreen/home/AddNewPlace';
 
-const String notificationPage = '/bottomScreen/home/NotificationPage'; //here
+// Route for the notifications page
+const String notificationPage = '/bottomScreen/home/NotificationPage'; // Route for the notification page
 
+// Route for event details screen under events section
 const String eventDetailsRoute =
     '/bottomScreen/home/EventsScreen/EventDetailsScreen';
 
+// Routes for different screens under the bottom navigation screen
 const String locationRoute = '/bottomScreen/LocationPage';
 const String communityRoute = '/bottomScreen/communityScreen';
 const String favoriteRoute = '/bottomScreen/favoriteScreen';
 const String profileRoute = '/bottomScreen/profileScreen';
+
+// Routes for place details and adding a report under place details
 const String placeDetailsRoute = '/bottomScreen/-/DetailsScreen';
 const String addReportRoute = '/bottomScreen/-/DetailsScreen/ReportPlace';
 
+// Route for viewing a specific question under the community section
 const String questionRoute = '/bottomScreen/communityScreen/question_view';
 
+// Routes for different screens under the profile section
 const String addedPlacesRoute = '/bottomScreen/profileScreen/AddedPlaces';
 const String aboutUsScreen = '/bottomScreen/profileScreen/AboutUsScreen';
-
 const String personalDetilsScreen =
     '/bottomScreen/profileScreen/PersdonalDetilsScreen';
-
 const String changeInfo = '/bottomScreen/profileScreen/ChangeInfo';
 const String signIn = '/LogIn';
 const String signUp = '/SingUp';
 const String deleteUserPage = '/bottomScreen/profileScreen/DeleteUser';
 
-
+// Route for viewing comments under place details
 const String commentsPlaceRoute = '/bottomScreen/-/DetailsScreen/CommentsPlace';
 
+// Route for requesting to be an expert under the profile section
 const String requestToBeExpertRoute = '/bottomScreen/Profile/RequestToBeExpert';
+
+// Route for editing a place under the profile section
 const String editPlaceRoute =
     '/bottomScreen/profileScreen/AddedPlaces/EditPlace';
 
-
-//admin screens
+// Routes for admin screens
 const String signInAdminRoute = '/signInAdmin';
 const String homeAdminRoute = '/signInAdmin/home';
 const String allReportsAdminRoute = '/signInAdmin/home/allReportsAdminRoute';
@@ -47,7 +59,7 @@ const String placeReportsAdminRoute =
 const String requestsAdminRoute = '/signInAdmin/home/requestsAdminRoute';
 const String requestPDFViewAdminRoute = '/signInAdmin/home/requestPDFViewAdminRoute';
 
+// Routes for admin managing places and events
 const String placesAdminRoute = '/signInAdmin/home/placesAdmin';
 const String editPlacesAdminRoute = '/signInAdmin/home/placesAdmin/placeInfo/editPlacesAdminRoute';
-
 const String eventsAdminRoute = '/signInAdmin/home/eventsAdmin';
--- lib/utils/theme_manager.dart
+++ lib/utils/theme_manager.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
+// ThemeManager class to manage the application's theme colors and styles
 class ThemeManager {
+  // Primary color used throughout the app
   static Color primary = const Color(0xff6E232F);
-  static Color second = const Color(0xffF7F3EE); //xffF0F2F6
 
-  static Color background = const Color(0xffF0F2F6); //xffF0F2F6
-  static Color textColor = const Color(0xff31221D); //31221D
-  static Color containerback = const Color(0xffE2D1B9); //E2D1B9
-  static Color favIcon = const Color(0xFFA74040); //0xFFA74040
+  // Secondary color used for backgrounds and other elements
+  static Color second = const Color(0xffF7F3EE);
 
+// Background color for the app
+  static Color background = const Color(0xffF0F2F6);
 
+  // Color for text elements
+  static Color textColor = const Color(0xff31221D);
+
+  // Background color for containers
+  static Color containerback = const Color(0xffE2D1B9);
+
+  // Color for favorite icons
+  static Color favIcon = const Color(0xFFA74040);
+
+  // Font family used in the app
   static String fontFamily = 'KohSantepheap';
+
+  // Default text style for the app
   static TextStyle textStyle = TextStyle(
       fontFamily: 'KohSantepheap',
       color: textColor,
       fontWeight: FontWeight.bold,
       fontSize: 16);
- static ButtonStyle buttonStyle= ButtonStyle(
-  shape: MaterialStateProperty.all(const StadiumBorder()),
-  backgroundColor:
-  MaterialStateProperty.all(ThemeManager.second));
+
+  // Default button style for the app
+  static ButtonStyle buttonStyle = ButtonStyle(
+      shape: MaterialStateProperty.all(const StadiumBorder()),
+      backgroundColor: MaterialStateProperty.all(ThemeManager.second));
 }
