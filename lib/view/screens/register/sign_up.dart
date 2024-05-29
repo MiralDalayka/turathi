37a60c5d48e68d7d69329daac9975840a000a647
Alaa  Jamal Abdalqader <162041062+AlaaJam@users.noreply.github.com>
2024-05-29 14:27:32 +0300

    delete Controllers ,Last Updates

--- lib/view/screens/register/sign_up.dart
+++ lib/view/screens/register/sign_up.dart
@@ -284,3 +284,61 @@ class _SingUpState extends State<SingUp> {
     }
   }
 }
+
+
+
+
+class SignUpController{
+
+
+   TextEditingController _email = TextEditingController();
+   TextEditingController _firstName = TextEditingController();
+   TextEditingController _secondName = TextEditingController();
+   TextEditingController _password = TextEditingController();
+   TextEditingController _phone = TextEditingController();
+   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  get formKey => _formKey;
+
+  set formKey(value) {
+    _formKey = value;
+
+
+   
+
+
+  }
+
+
+  TextEditingController get phone => _phone;
+
+  set phone(TextEditingController value) {
+    _phone = value;
+  }
+
+  TextEditingController get password => _password;
+
+  set password(TextEditingController value) {
+    _password = value;
+  }
+
+  TextEditingController get secondName => _secondName;
+
+  set secondName(TextEditingController value) {
+    _secondName = value;
+  }
+
+  TextEditingController get firstName => _firstName;
+
+  set firstName(TextEditingController value) {
+    _firstName = value;
+  }
+
+  TextEditingController get email => _email;
+
+  set email(TextEditingController value) {
+    _email = value;
+  }
+
+
+}
